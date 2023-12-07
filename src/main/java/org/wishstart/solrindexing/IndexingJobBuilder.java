109e5a252abd3d496e1c7a56fead920e13db751b
judahw <judahw@bhphoto.com>
2023-12-07 17:42:49 -0500

    generic functions to simplify creating readers and steps

--- src/main/java/org/wishstart/solrindexing/IndexingJobBuilder.java
+++ src/main/java/org/wishstart/solrindexing/IndexingJobBuilder.java
@@ -1,6 +1,5 @@
 package org.wishstart.solrindexing;
 
-import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
@@ -16,28 +15,22 @@ import org.springframework.batch.core.job.builder.JobBuilder;
 import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.batch.core.step.builder.StepBuilder;
 import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.item.ItemProcessor;
 import org.springframework.batch.item.ItemReader;
+import org.springframework.batch.item.ItemWriter;
 import org.springframework.batch.item.database.JdbcCursorItemReader;
-import org.springframework.batch.item.file.FlatFileItemReader;
-import org.springframework.batch.item.file.LineMapper;
-import org.springframework.batch.item.file.builder.FlatFileItemReaderBuilder;
-import org.springframework.batch.item.file.transform.DelimitedLineTokenizer;
-import org.springframework.batch.item.file.transform.FieldSet;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.FileSystemResource;
 import org.springframework.jdbc.core.ColumnMapRowMapper;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.transaction.PlatformTransactionManager;
 
 import lombok.Getter;
 import lombok.Setter;
-import lombok.extern.slf4j.Slf4j;
 
 @Configuration
 @ConfigurationProperties(prefix = "solr")
-@Slf4j
 public class IndexingJobBuilder {
 
 	@Getter @Setter
@@ -72,60 +65,29 @@ public class IndexingJobBuilder {
 
 	@Bean
 	public Step step2(JobRepository jobRepository, DataSourceTransactionManager transactionManager,
-					ItemReader<Map<String, Object>> itemReader, MapToSolrDocumentProcessor processor, SolrItemWriter writer) {
-		return new StepBuilder("step_add_items", jobRepository)
-			.<Map<String, Object>,SolrInputDocument> chunk(10, transactionManager)
-			.reader(itemReader)
-			.processor(processor)
-			.writer(writer)
-			.build();
+			ItemReader<Map<String, Object>> itemReader, MapToSolrDocumentProcessor processor, SolrItemWriter writer) {
+		return stepBuilder("step_add_items", jobRepository, transactionManager, itemReader, processor, writer);	
 	}
 
 	@Bean
 	public Step step3(JobRepository jobRepository, DataSourceTransactionManager transactionManager,
-					ItemReader<Map<String, Object>> brandReader, MapToSolrDocumentUpdateProcessor processor, SolrItemWriter writer) {
-		return new StepBuilder("step_add_brands", jobRepository)
-			.<Map<String, Object>,SolrInputDocument> chunk(10, transactionManager)
-			.reader(brandReader)
-			.processor(processor)
-			.writer(writer)
-			.build();
-	}
+			ItemReader<Map<String, Object>> brandReader, MapToSolrDocumentUpdateProcessor processor, SolrItemWriter writer) {
+		return stepBuilder("step_add_brands", jobRepository, transactionManager, brandReader, processor, writer);	}
 
 	@Bean
 	public Step step4(JobRepository jobRepository, DataSourceTransactionManager transactionManager,
-					ItemReader<Map<String, Object>> categoryReader, MapToSolrDocumentUpdateProcessor processor, SolrItemWriter writer) {
-		return new StepBuilder("step_add_categories", jobRepository)
-			.<Map<String, Object>,SolrInputDocument> chunk(10, transactionManager)
-			.reader(categoryReader)
-			.processor(processor)
-			.writer(writer)
-			.build();
+			ItemReader<Map<String, Object>> categoryReader, MapToSolrDocumentUpdateProcessor processor, SolrItemWriter writer) {
+		return stepBuilder("step_add_categories", jobRepository, transactionManager, categoryReader, processor, writer);
 	}
 
-	private String [] names = {"availability","condition","issale","shipping","manufacturernumber","name","weight","price","asin","id","url","image_url","brand_id"};
 
-	@Bean
-	public FlatFileItemReader<Map<String, Object>> reader() {
-		log.info("Building item reader");
-		return new FlatFileItemReaderBuilder<Map<String, Object>>()
-			.name("itemReader")
-			.resource(new FileSystemResource("items.csv"))
-			.linesToSkip(1)
-			.lineMapper(new LineMapper<Map<String, Object>>() {
-				DelimitedLineTokenizer tokenizer = new DelimitedLineTokenizer(",");
-
-				@Override
-				public Map<String, Object> mapLine(String line, int lineNumber) throws Exception {
-					if(!tokenizer.hasNames()) {
-						tokenizer.setNames(names);
-					}
-					FieldSet fs = tokenizer.tokenize(line);
-					Map<String, Object> map = new HashMap<>();
-					fs.getProperties().entrySet().forEach(e -> map.put((String)e.getKey(), e.getValue()));
-					return map;
-				}
-			})
+	private Step stepBuilder(String name, JobRepository jobRepository, DataSourceTransactionManager transactionManager,
+		ItemReader<Map<String, Object>> reader, ItemProcessor processor, ItemWriter writer) {
+		return new StepBuilder(name, jobRepository)
+			.<Map<String, Object>,SolrInputDocument> chunk(100, transactionManager)
+			.reader(reader)
+			.processor(processor)
+			.writer(writer)
 			.build();
 	}
 
@@ -134,12 +96,7 @@ public class IndexingJobBuilder {
 		String sql = """
 				select * from items
 				""";
-		JdbcCursorItemReader<Map<String, Object>> reader = new JdbcCursorItemReader<>();
-		reader.setDataSource(dataSource);
-		reader.setSql(sql);
-		reader.setFetchSize(1000);
-		reader.setRowMapper(new ColumnMapRowMapper());
-		return reader;
+		return readerBuilder(dataSource, sql);
 	}
 
 	@Bean
@@ -149,12 +106,7 @@ public class IndexingJobBuilder {
 				from items
 					join brands using (brand_id)
 				""";
-		JdbcCursorItemReader<Map<String, Object>> reader = new JdbcCursorItemReader<>();
-		reader.setDataSource(dataSource);
-		reader.setSql(sql);
-		reader.setFetchSize(1000);
-		reader.setRowMapper(new ColumnMapRowMapper());
-		return reader;
+		return readerBuilder(dataSource, sql);
 	}
 
 	@Bean
@@ -164,11 +116,16 @@ public class IndexingJobBuilder {
 				from item_categories 
 					join categories using(category_id)
 				""";
+		return readerBuilder(dataSource, sql);
+	}
+
+	private JdbcCursorItemReader<Map<String, Object>> readerBuilder(DataSource dataSource, String sql) {
 		JdbcCursorItemReader<Map<String, Object>> reader = new JdbcCursorItemReader<>();
 		reader.setDataSource(dataSource);
 		reader.setSql(sql);
 		reader.setFetchSize(1000);
 		reader.setRowMapper(new ColumnMapRowMapper());
 		return reader;
+		
 	}
 }
